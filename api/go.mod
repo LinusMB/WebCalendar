e835777280062bd194be85df02e8426aa7b24618
Linus Buddrus <buddruslinus@gmail.com>
2023-08-19 18:08:16 +0200

    add middleware for cors and logging

--- api/go.mod
+++ api/go.mod
@@ -10,8 +10,10 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-testfixtures/testfixtures/v3 v3.8.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
--- api/internal/router/router.go
+++ api/internal/router/router.go
@@ -3,13 +3,33 @@ package router
 import (
 	"api/internal/controller"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
 )
 
+type MiddlewareFunc func(h http.Handler) http.Handler
+
+func Use(r http.Handler, middleware ...MiddlewareFunc) http.Handler {
+	for i := range middleware {
+		r = middleware[i](r)
+	}
+	return r
+}
+
 func New(controller *controller.Controller, config *viper.Viper) http.Handler {
 	r := mux.NewRouter()
+
+	corsMiddleware := handlers.CORS(
+		handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+	)
+
+	logMiddleware := func(h http.Handler) http.Handler {
+		return handlers.LoggingHandler(os.Stdout, h)
+	}
 
 	r.HandleFunc("/api/events", controller.GetEventsByDate).Methods(http.MethodGet).Queries("start", "{start:.*}", "end", "{end:.*}")
 	r.HandleFunc("/api/events", controller.GetAllEvents).Methods(http.MethodGet)
@@ -19,5 +39,5 @@ func New(controller *controller.Controller, config *viper.Viper) http.Handler {
 	r.HandleFunc("/api/events/{uuid}", controller.DeleteEvent).Methods(http.MethodDelete)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir(config.GetString("frontend.path"))))
 
-	return r
+	return Use(r, corsMiddleware, logMiddleware)
 }
