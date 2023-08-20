c3f6dd8ce626adac4d8fa3443ba6e4385e90eb7d
Linus Buddrus <buddruslinus@gmail.com>
2023-08-20 20:12:07 +0200

    add handler for retrieving events by day, week, month

--- api/internal/controller/event.go
+++ api/internal/controller/event.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -34,6 +35,86 @@ func (c *Controller) GetEventsByDate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	evts, err := c.storage.Event.GetByDate(start, end)
+	if err != nil {
+		writeMsg(w, http.StatusInternalServerError, "message", "data access failure")
+		return
+	}
+	writeJSON(w, http.StatusOK, evts)
+}
+
+func (c *Controller) GetEventsByDay(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	location, err := time.LoadLocation(vars["tz"])
+	if err != nil {
+		writeMsg(w, http.StatusBadRequest, "message", err.Error())
+		return
+	}
+	start, err := time.ParseInLocation(time.DateOnly, vars["date"], location)
+	if err != nil {
+		writeMsg(w, http.StatusBadRequest, "message", err.Error())
+		return
+	}
+	startUTC := start.UTC()
+	endUTC := startUTC.AddDate(0, 0, 1)
+	evts, err := c.storage.Event.GetByDate(startUTC, endUTC)
+	if err != nil {
+		writeMsg(w, http.StatusInternalServerError, "message", "data access failure")
+		return
+	}
+	writeJSON(w, http.StatusOK, evts)
+}
+
+func (c *Controller) GetEventsByWeek(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	location, err := time.LoadLocation(vars["tz"])
+	if err != nil {
+		writeMsg(w, http.StatusBadRequest, "message", err.Error())
+		return
+	}
+	year, err := strconv.Atoi(vars["year"])
+	if err != nil {
+		writeMsg(w, http.StatusBadRequest, "message", err.Error())
+		return
+	}
+	week, err := strconv.Atoi(vars["week"])
+	if err != nil {
+		writeMsg(w, http.StatusBadRequest, "message", err.Error())
+		return
+	}
+	t := time.Date(year, 1, 1, 0, 0, 0, 0, location)
+	for t.Weekday() != time.Monday {
+		t = t.AddDate(0, 0, 1)
+	}
+	startUTC := t.Add(time.Duration(week-1) * 7 * 24 * time.Hour).UTC()
+	endUTC := startUTC.AddDate(0, 0, 7)
+	evts, err := c.storage.Event.GetByDate(startUTC, endUTC)
+	if err != nil {
+		writeMsg(w, http.StatusInternalServerError, "message", "data access failure")
+		return
+	}
+	writeJSON(w, http.StatusOK, evts)
+}
+
+func (c *Controller) GetEventsByMonth(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	location, err := time.LoadLocation(vars["tz"])
+	if err != nil {
+		writeMsg(w, http.StatusBadRequest, "message", err.Error())
+		return
+	}
+	year, err := strconv.Atoi(vars["year"])
+	if err != nil {
+		writeMsg(w, http.StatusBadRequest, "message", err.Error())
+		return
+	}
+	month, err := strconv.Atoi(vars["month"])
+	if err != nil {
+		writeMsg(w, http.StatusBadRequest, "message", err.Error())
+		return
+	}
+	startUTC := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, location).UTC()
+	endUTC := startUTC.AddDate(0, 1, 0)
+	evts, err := c.storage.Event.GetByDate(startUTC, endUTC)
 	if err != nil {
 		writeMsg(w, http.StatusInternalServerError, "message", "data access failure")
 		return
--- api/internal/router/router.go
+++ api/internal/router/router.go
@@ -32,6 +32,9 @@ func New(controller *controller.Controller, config *viper.Viper) http.Handler {
 	}
 
 	r.HandleFunc("/api/events", controller.GetEventsByDate).Methods(http.MethodGet).Queries("start", "{start:.*}", "end", "{end:.*}")
+	r.HandleFunc("/api/events/day", controller.GetEventsByDay).Methods(http.MethodGet).Queries("date", "{date:.*}", "tz", "{tz:.*}")
+	r.HandleFunc("/api/events/week", controller.GetEventsByWeek).Methods(http.MethodGet).Queries("year", "{year:.*}", "week", "{week:.*}", "tz", "{tz:.*}")
+	r.HandleFunc("/api/events/month", controller.GetEventsByMonth).Methods(http.MethodGet).Queries("year", "{year:.*}", "month", "{month:.*}", "tz", "{tz:.*}")
 	r.HandleFunc("/api/events", controller.GetAllEvents).Methods(http.MethodGet)
 	r.HandleFunc("/api/events", controller.CreateEvent).Methods(http.MethodPost)
 	r.HandleFunc("/api/events/{uuid}", controller.GetEvent).Methods(http.MethodGet)
