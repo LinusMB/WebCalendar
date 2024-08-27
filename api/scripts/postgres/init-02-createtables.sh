d6bb1db14eb529d055d5a04e488faabe50ba9346
Linus Buddrus <buddruslinus@gmail.com>
2024-08-27 18:45:30 +0200

    add overlap constraint to events table

--- api/scripts/postgres/init-02-createtables.sh
+++ api/scripts/postgres/init-02-createtables.sh
@@ -6,6 +6,8 @@ db_name="calendar"
 db_name_test="${db_name}_test"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$db_name" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS btree_gist;
+    
     CREATE TABLE events (
       id          SERIAL PRIMARY KEY,
       uuid        VARCHAR(64) NOT NULL UNIQUE,
@@ -15,9 +17,15 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$db_name" <<-EOSQL
       date_to     TIMESTAMP NOT NULL,
       created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
+
+    ALTER TABLE events ADD CONSTRAINT event_overlap EXCLUDE USING gist (
+        tsrange(date_from, date_to, '[)') WITH &&
+    );
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$db_name_test" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS btree_gist;
+
     CREATE TABLE events (
       id          SERIAL PRIMARY KEY,
       uuid        VARCHAR(64) NOT NULL UNIQUE,
@@ -26,5 +34,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$db_name_test" <<-
       date_from   TIMESTAMP NOT NULL,
       date_to     TIMESTAMP NOT NULL,
       created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+    ALTER TABLE events ADD CONSTRAINT event_overlap EXCLUDE USING gist (
+        tsrange(date_from, date_to, '[)') WITH &&
     );
 EOSQL
